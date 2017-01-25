dc2c15f3a45397c8cf407b28d04d49ef8361e74b
杉原 <shinta_16n1200325@nnn.ed.jp>
2017-01-25 16:50:29 +0900

    Delete unneccesary code

--- models/markdown_processor/img_filter.rb
+++ models/markdown_processor/img_filter.rb
@@ -1,7 +1,6 @@
 class MarkdownProcessor
   class ImgFilter < HTML::Pipeline::Filter
     def call
-      puts "hoge"
       doc.search('.//text()').each do |node|
         html = node.to_html.gsub(/(https|http)?:\/\/.+\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
         %|<img src="#{match}" alt=""/>|
