f9b6a1e1c5e52bfe1261203691ad0eb97bd162d6
杉原 <shinta_16n1200325@nnn.ed.jp>
2017-01-26 17:14:22 +0900

    Modify css

--- models/markdown_processor/img_filter.rb
+++ models/markdown_processor/img_filter.rb
@@ -3,7 +3,7 @@ class MarkdownProcessor
     def call
       doc.search('.//text()').each do |node|
         html = node.to_html.gsub(/(https|http)?:\/\/.+\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
-        %|<img src="#{match}" alt=""/>|
+        %|<img src="#{match}" alt="" class="ui medium bordered image"/>|
         end
         node.replace(html)
       end
